bb6e4fb7ce9823b4f5af5ce47f13a6b307b71b2d
dipto0321 <diptokmk47@gmail.com>
2019-03-08 11:30:10 +0600

    Reverse function test

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const capitalize = (str) => {
   return str.slice(0, 1).toUpperCase() + str.slice(1);
 };
 
+const reverse = (str) => str.split('').reverse().join('');
+
 module.exports = {
   capitalize,
+  reverse,
 }
--- test/index.test.js
+++ test/index.test.js
@@ -4,3 +4,8 @@ test('capitalize', () => {
   expect(index.capitalize("demo")).toBe("Demo");
   expect(index.capitalize("book")).toBe("Book");
 });
+
+test('reverse', () => {
+  expect(index.reverse("demo")).toBe("omed");
+  expect(index.reverse("hear")).toBe("raeh");
+});
