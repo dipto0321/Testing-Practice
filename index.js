9c58a36f5e639990aec933993eeaea0fd6a081a4
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-03-08 13:39:21 +0800

    Write Test and code for calculator object w/ corresponding methods

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ const capitalize = (str) => {
   return str.slice(0, 1).toUpperCase() + str.slice(1);
 };
 
-const reverse = (str) => str.split('').reverse().join('');
+const reverse = str => str.split('').reverse().join('');
+
+const calculator = (() => ({
+  add(a, b) {
+    return a + b;
+  },
+  subtract(a, b) {
+    return a - b;
+  },
+  multiply(a, b) {
+    return a * b;
+  },
+  divide(a, b) {
+    return a / b;
+  },
+}))();
 
 module.exports = {
   capitalize,
   reverse,
-}
+  calculator,
+};
--- test/index.test.js
+++ test/index.test.js
@@ -9,3 +9,18 @@ test('reverse', () => {
   expect(index.reverse("demo")).toBe("omed");
   expect(index.reverse("hear")).toBe("raeh");
 });
+
+describe('calculator', () => {
+  test('add', () => {
+    expect(index.calculator.add(3, 4)).toBe(7);
+  });
+  test('subtract', () => {
+    expect(index.calculator.subtract(4, 3)).toBe(1);
+  });
+  test('multiply', () => {
+    expect(index.calculator.multiply(4, 3)).toBe(12);
+  });
+  test('divide', () => {
+    expect(index.calculator.divide(4, 2)).toBe(2);
+  });
+});
